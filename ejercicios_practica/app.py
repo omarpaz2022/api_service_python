dcd41d6d30ced40b8e54888ca3e34ca28d36a09b
omarpaz2022 <107511393+omarpaz2022@users.noreply.github.com>
2023-03-12 12:27:28 -0300

    ejercicio de practico resuelto

--- ejercicios_practica/app.py
+++ ejercicios_practica/app.py
@@ -54,18 +54,29 @@ def personas():
         # Alumno:
         # Implementar la captura de limit y offset de los argumentos
         # de la URL
-        # limit = ...
-        # offset = ....
-
-        # Debe verificar si el limit y offset son válidos cuando
-        # no son especificados en la URL
+        limit_str = str(request.args.get('limit'))
+        offset_str = str(request.args.get('offset'))
 
         limit = 0
         offset = 0
 
-        result = persona.report(limit=limit, offset=offset)
-        return jsonify(result)
+        # Debe verificar si el limit y offset son válidos cuando
+        # no son especificados en la URL
+
+        if(limit_str is not None) and (limit_str.isdigit()):
+            limit = int(limit_str)
+
+        if(offset_str is not None) and (offset_str.isdigit()):
+            offset = int(offset_str)
+        
+        # obtener el reporte
+        data = persona.report(limit=limit, offset=offset)  
+
+        # Transformar json a json string para enviar al HTML
+        print(data)
+        return jsonify(data) 
     except:
+        print("Error!!!")
         return jsonify({'trace': traceback.format_exc()})
 
 
@@ -84,8 +95,20 @@ def registro():
             # Alumno: descomentar la linea persona.insert una vez implementado
             # lo anterior:
             # persona.insert(name, int(age))
+            name = str(request.form.get('name'))
+            age = str(request.form.get('age'))
+
+            if(name is None or age is None or age.isdigit() is False):
+            # Datos ingresados incorrectos
+                     return Response(status=400)
+            
+            persona.insert(name, int(age))
+
+
             return Response(status=200)
+    
         except:
+    
             return jsonify({'trace': traceback.format_exc()})
 
 
@@ -106,11 +129,11 @@ def comparativa():
 
         # Descomentar luego de haber implementado su función en persona.py:
 
-        # x, y = persona.dashboard()
-        # image_html = utils.graficar(x, y)
-        # return Response(image_html.getvalue(), mimetype='image/png')
+         x, y = persona.dashboard()
+         image_html = utils.graficar(x, y)
+         return Response(image_html.getvalue(), mimetype='image/png')
 
-        return "Alumno --> Realice la implementacion"
+        #return "Alumno --> Realice la implementacion"
     except:
         return jsonify({'trace': traceback.format_exc()})
 
