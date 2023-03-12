950f69c8b3ebfcbe85d93f3810bf7e33af443422
omarpaz2022 <107511393+omarpaz2022@users.noreply.github.com>
2023-03-12 12:28:14 -0300

    ejercicio de practico resuelto

--- ejercicios_practica/persona.py
+++ ejercicios_practica/persona.py
@@ -53,25 +53,36 @@ def report(limit=0, offset=0):
 
     return json_result_list
 
+def dashboard():
+    query = db.session.query(Persona)
+    x= []
+    y= []
+    for person in query:
+            x.append(person.name)
+            y.append(person.age)
+    return (x , y)
+
+
 
 if __name__ == "__main__":
-    print("Test del modulo heart.py")
+    #print("Test del modulo heart.py")
+      print("Inicio")
 
     # Crear una aplicación Flask para testing
     # y una base de datos fantasma (auxiliar o dummy)
     # Referencia:
     # https://stackoverflow.com/questions/17791571/how-can-i-test-a-flask-application-which-uses-sqlalchemy
-    app = Flask(__name__)
-    app.config['TESTING'] = True
-    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///testdatabase.db"
+    #app = Flask(__name__)
+    #app.config['TESTING'] = True
+    #app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///testdatabase.db"
     # Bindear la DB con nuestra app Flask
-    db.init_app(app)
-    app.app_context().push()
+    #db.init_app(app)
+    #app.app_context().push()
 
-    db.create_all()
+    #db.create_all()
 
     # Aquí se puede ensayar todo lo que necesitemos con nuestra DB
     # ...
 
-    db.session.remove()
-    db.drop_all()
+    #db.session.remove()
+    #db.drop_all()
